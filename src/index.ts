4dc62e92361bd6cfbbfa514bd9d50bb947cd6ef5
xjf7711@qq.com <xjf7711@qq.com>
2020-04-08 11:58:03 +0800

    edit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 /*
  * @功能描述: 
- * @作者: 高云蛟
- * @Date: 2019-08-25 17:32:46
  */
 import { greeting } from './greeter'
 let str: string = 'Hello TypeScript .... '
--- src/math.ts
+++ src/math.ts
@@ -1,7 +1,5 @@
 /*
  * @功能描述: 
- * @作者: 高云蛟
- * @Date: 2019-08-27 18:07:33
  */
 function add(a: number, b: number) {
     return a + b;

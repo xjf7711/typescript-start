3b2cbff05b32aff6bf9a98b57534d3a4afc94b8f
xu jianfeng <jianfeng.xu@cssca.com>
2022-03-07 15:07:10 +0800

    edit

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,6 +1,16 @@
 /*
- * @功能描述:
+ *
  */
+// const webpack = require("webpack");
+
+// webpack打包文件体积和依赖关系的可视化
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 module.exports = {
-    devtool: 'eval-cheap-module-source-map' // 忽略列信息的ts源码
-}
+  plugins: [
+    // webpack 分析页面
+    // new BundleAnalyzerPlugin({ analyzerPort: 3333 }),
+    //   new webpack.NamedModulesPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
+  ],
+  devtool: 'eval-cheap-module-source-map' // 忽略列信息的ts源码
+};
--- src/greeter.ts
+++ src/greeter.ts
@@ -1,3 +1,3 @@
 export function greeting() {
-  console.log('hello ...')
+  console.log('hello ...');
 }
